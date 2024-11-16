1327d66fe3ba50fd50b565e6c66fa93b27c16ee7
Sepelire <arthurpan2001@mail.ru>
2024-11-16 23:23:06 +0500

    Complete: 2nd task

---
+++ not_multiplication_table.py
@@ -0,0 +1,11 @@
+# Таблица умножения
+n = int(input())
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print(f'{j} * {i} = {j * i}')
+
+# 2
+# 1 * 1 = 1
+# 2 * 1 = 2
+# 1 * 2 = 2
+# 2 * 2 = 4
