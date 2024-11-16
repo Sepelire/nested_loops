cc6148612d7b60dd794ace6ab0c0cd706eb5f9dc
Sepelire <arthurpan2001@mail.ru>
2024-11-16 23:20:36 +0500

    Complete: first task

---
+++ multiplication_table.py
@@ -0,0 +1,11 @@
+# таблица умножения
+dimension = int(input())
+for i in range(1, dimension + 1):
+    for j in range(1, dimension + 1):
+        print(i * j, end=' ')
+    print('')
+
+# 3
+# 1 2 3 
+# 2 4 6 
+# 3 6 9 
